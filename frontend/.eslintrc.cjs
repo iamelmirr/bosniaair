06df1dd328ad56b790207f43344e6c7f19f9db59
Elmir <besirovicelmir36@gmail.com>
2025-09-27 20:01:42 +0200

    refactor: Update ESLint configuration to include TypeScript parser and enhance environment settings

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,7 +1,21 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   extends: ['next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   rules: {
     'react-hooks/exhaustive-deps': 'warn',
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
 }
